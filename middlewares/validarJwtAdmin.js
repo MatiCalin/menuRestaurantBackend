1d20925eef9b046b222e1c0482ab00164c7b3cd2
Matias Calin <maticalin@gmail.com>
2023-03-19 19:33:04 -0300

    funcion ruta protegida hecha

---
+++ middlewares/validarJwtAdmin.js
@@ -0,0 +1,34 @@
+const express = require ("express");
+const jwt = require ("jsonwebtoken");
+
+const validarJWTAdmin = (req, res, next) =>{
+    const token = req.header("x-token");
+
+    if(!token){
+        return res.status(401).json({
+            ok: false,
+            msg: "No hay token en la peticion",
+        })
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_JWT);
+        if (payload.usuario === "usuario"){
+            return res.status(404).json({
+                ok: false,
+                msg: "usuario no tiene rol de admin",
+            });
+        }
+    } catch (error) {
+        return res.status(401).json({
+            ok: false,
+            msg: "token no valido",
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validarJWTAdmin,
+}
